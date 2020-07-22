a09b1968f4737b32c1cd5c447ff1a33f7b9b758e
Andrey Zhizhov <andrey_learning@mail.ru>
2020-07-22 21:49:43 +0500

    Создал класс Point

---
+++ Snake/Point.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    /// <summary>
+    /// клас описывающий точку
+    /// и выводит её на экран
+    /// </summary>
+    class Point
+    {
+        public int x;
+        public int y;
+        public char symbol;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(symbol);
+        }
+    }
+}
---
+++ Snake/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point();
+
+            // создаем точку в координатах
+            p1.x = 1;
+            p1.y = 3;
+            p1.symbol = '*';
+
+            // выводим на экран
+            p1.Draw();
+
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.symbol = '#';
+
+            p2.Draw();
+
+            Console.ReadKey();
+        }
+    }
+}
